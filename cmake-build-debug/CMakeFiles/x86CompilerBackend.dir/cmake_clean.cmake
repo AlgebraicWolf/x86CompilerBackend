c57af47b01357ce383ee1840b9ad99da7c7f1a5e
Alexey <volkov.aa@phystech.edu>
2020-05-15 17:26:47 +0300

    Inited CLion Project

---
+++ cmake-build-debug/CMakeFiles/x86CompilerBackend.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/x86CompilerBackend.dir/main.cpp.o"
+  "x86CompilerBackend"
+  "x86CompilerBackend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/x86CompilerBackend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/x86CompilerBackend.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for x86CompilerBackend.
+# This may be replaced when dependencies are built.
